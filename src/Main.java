4e313af73b65b41af8a4367ccacc91d6f18fa03e
e.melvin1 <elliott.neil.melvin@gmail.com>
2025-04-08 11:31:32 -0600

    Everything

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurant.Ex2_Main;
 import Ex3_MusicFestival.Ex3_Main;
-import TwoD_Arrays.Ex1_Golf;
-import TwoD_Arrays.Ex2_TicTacToe;
+import Family_Tree.FamilyTreeMain;
+import String_Methods.As1_Main;
+import TwoD_Arrays.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,15 @@ public class Main {
 
 //        Ex1_Main.run();
 //        Ex2_Main.run();
-        Ex3_Main.run();
+//        Ex3_Main.run();
 //        Ex1_Golf.run();
 //        Ex2_TicTacToe.run();
+//        Ex3_Sudoku.run();
+//        As1_Bingo.run();
+//        As2_Vigenere.run();
+//        FamilyTreeMain.run();
+        As1_Main.run();
     }
+
 
 }
